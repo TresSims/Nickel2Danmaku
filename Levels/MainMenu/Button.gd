76b9b0a6031f1116543600e62e63fa6db4111671
nickel2sulfate <nsulfate@gmail.com>
2023-03-25 19:29:35 -0400

    Created base UI Main Menu Theme

--- Levels/MainMenu/Button.gd
+++ Levels/MainMenu/Button.gd
@@ -1,8 +1,8 @@
 extends Button
 
 func _ready():
-        print("Button Ready")
-        self.pressed.connect(self._button_pressed)
+		print("Button Ready")
+		self.pressed.connect(self._button_pressed)
 
 func _button_pressed():
-        get_tree().change_scene_to_file("res://Levels/Level1/Level1.tscn")
+		get_tree().change_scene_to_file("res://Levels/Level1/Level1.tscn")
