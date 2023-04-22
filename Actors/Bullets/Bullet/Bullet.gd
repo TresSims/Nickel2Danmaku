647b39fe992c960102a5e2304af1684d419a5b0f
Tres Sims <tres.sims95@gmail.com>
2023-04-21 21:16:51 -0400

    Set up bullet randomization and scene inheritance

--- Actors/Bullets/Bullet/Bullet.gd
+++ Actors/Bullets/Bullet/Bullet.gd
@@ -7,13 +7,8 @@ var speed = 10
 var dir = Vector2(0, 1)
 
 # Sprite Variables
-@onready var _sprite = $Sprite2D
+@onready var _sprite = $AnimatedSprite2D
 var rand = RandomNumberGenerator.new()
-var sprites = 4
-var sprite_y = 0
-var sprite_size = Vector2(64, 64)
-var sprite_start = Vector2(0, 0)
-var sprite_position = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,22 +22,21 @@ func setup_colision():
 
 
 func setup_sprite():
+    _sprite.pause()
     # set initial rotation
     _sprite.rotation = dir.angle()
 
     # Select random sprite from availabe
-    var chosen_sprite = rand.randi_range(0, sprites-1)
+    var chosen_sprite = rand.randi_range(0, _sprite.sprite_frames.get_frame_count(_sprite.animation)-1)
     print(chosen_sprite)
-    sprite_position = sprite_start
-    sprite_position.x = sprite_start.x + sprite_size.x * chosen_sprite
 
-    _sprite.region_rect.position = sprite_position
+    _sprite.frame = chosen_sprite
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     move_and_collide(self.dir*self.speed)
-    setup_sprite()
     _sprite.rotation = dir.angle()
 
 func _on_body_entered(body:Node):
--- Actors/Bullets/Bullet_Glitch/Bullet_Glitch.gd
+++ Actors/Bullets/Bullet_Glitch/Bullet_Glitch.gd
@@ -4,10 +4,6 @@ func _ready():
     self.speed = 10
     self.dir = Vector2(0, -1)
 
-    self.sprites = 4
-
-    self.sprite_start = Vector2(0, 0)
-
     self.setup_colision()
 
     self.setup_sprite()
